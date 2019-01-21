716203357045fdb26f2b98e18a7372b2900cbfee
Tianyi <42670338+tysng@users.noreply.github.com>
2019-01-21 15:21:50 +0800

    Completed filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import token from './token.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import { all } from 'q';
 
-// const key = get_token()[0]; // key is constant
-//***REMOVED*** = get_token()[1];
-
-/* this code doesn't really work 
-
-
-function getModules() {
-    // returns an array of modules 
-}
-
-*/
-// function getObject(url) {
-//     request(url, function (error, response, body) {
-//         console.log('error:', error); // Print the error if one occurred
-//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//         console.log('body:', body); // Print the HTML for the Google homepage.
-//     });
-// }
-
-
-
 const key = "EKCnWpNMgPfzGr9psFhqq"; // key is constant
-const token = "5ADC923208A41CDD38E28F8D97383F3B261C7CCDCB69BE826B9F51361BF9404FC9E5E524CA57B3D573D084E642AF16BE5311FE0229D135E00F26244B4FC3C8BE23D365DA846ABD06F24CE7405BE9A1D4B3EEF513DD0EF0782AB019995E1230B3DAC6D087A12F11464AC8BA5ADF98E3478CA2CB9E77969FB83609BFBC9FEE2826A6855D3CFE4FEF5F39A4AA8764FEAD03639EFDD76A2179F0FB118F8A40D16BE734D73C8F4EF99C5E2A4C1371918C1D064EE587C68AEDBD5F5982B44ABEB9980BF29FA6764A9802F4C6A8B9170320B97C209F233A9DC9F2C09DD469B4CD0B004F8044213AB39D35A52CD1D69E3923F948";
 
 const module_call = "https://ivle.nus.edu.sg/api/Lapi.svc/Modules?APIKey=" +
     key + "&AuthToken=" +
     token + "&Duration=10&IncludeAllInfo=false";
-
-
-// getObject(module_call);
-
-
-
-// Assume we already have a list of modules
-const module_list = [
-    'MA1521',
-    'CS1101S',
-    'CS1231',
-];
-
-// Assume we also have the namelist which has the same order as module_list
-const name_lists = [
-    [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-    ],
-    [
-        "1",
-        "2",
-        "5",
-        "6",
-        "7",
-    ],
-    [
-        "1",
-        "3",
-        "4",
-        "6",
-    ],
-]
 
 function Toggle(props) {
     return (
@@ -84,10 +25,13 @@ class Content extends Component {
     findIntersection() {
         // refer to the global name_list var, and compare with buttonState
         // return the intersection in an array
+        const name_obj = this.props.nameList;
+        const id_list = this.props.id;
         const selectedLists = []
         for (let i = 0; i < this.props.buttonState.length; i++) {
             if (this.props.buttonState[i]) {
-                selectedLists.push(name_lists[i]);
+                const id = id_list[i];
+                selectedLists.push(name_obj[id]);
             }
         }
 
@@ -104,13 +48,11 @@ class Content extends Component {
     }
 
     render() {
-
         const module_buttons = this.props.courseCode.map(
             (value, index) => {
                 return (
                     // <button key={index}>{value}</button>
                     <Toggle
-
                         onClick={() => this.props.onClick(index)}
                         isToggleOn={this.props.buttonState[index]}
                         content={this.props.courseCode[index]}
@@ -121,6 +63,7 @@ class Content extends Component {
 
         return (
             <div>
+                <button onClick={() => this.props.getNamelist()}>Test</button>
                 <div className="buttons">
                     {module_buttons}
                 </div>
@@ -130,7 +73,6 @@ class Content extends Component {
                 </div>
                 <div>
                     <a href={module_call}>Press here for magic</a>
-                    {/* {this.state.moduleResponse.Results.length} */}
                 </div>
             </div>
         )
@@ -145,30 +87,56 @@ class App extends Component {
         this.state = {
             isInitiated: false,
             buttonState: null,
-            moduleID: null,
+            ID: null,
             courseCode: null,
             moduleResponse: null,
+            nameList: {},
         }
+
+        this.getNamelist = this.getNamelist.bind(this);
+
     }
 
     initialize(url) {
-        console.log(url);
         return fetch(url)
             .then(resp => resp.json())
             .then(data => {
                 console.log(data);
-                console.log(data.Results.length);
-                this.setState(
-                    {
+                this.setState((state) => {
+                    return {
                         isInitiated: true,
                         buttonState: Array(data.Results.length).fill(false),
-                        courseCode: data.Results.map(obj => obj.CourseCode)
-                        // moduleID: ,
-
-                    });
-            }
-            );
+                        courseCode: data.Results.map(obj => obj.CourseCode),
+                        ID: data.Results.map(obj => obj.ID),
+                    };
+                }
+                );
+            })
     }
+
+
+    getNamelist() {
+        const urls = id => "https://ivle.nus.edu.sg/API/Lapi.svc/Class_Roster?APIKey=" + key + "&AuthToken=" + token + "&CourseID=" + id
+
+
+        this.state.ID.map(
+            id => fetch(urls(id))
+                .then(resp => resp.json())
+                .then(data => this.setState(
+                    (state) => {
+                        console.log(data);
+
+                        let nameList = { ...state.nameList }
+                        nameList[id] = data.Results.map(obj => obj.Name)
+                        // const nameList = state.nameList.concat(
+                        //     [{ [id]: data.Results.map(i => i.Name) }])
+                        return { nameList }
+
+                    }
+                ))
+        )
+    }
+
 
     handlesClick(i) {
         const buttonState_copy = this.state.buttonState.slice();
@@ -194,10 +162,13 @@ class App extends Component {
                             buttonState={this.state.buttonState}
                             courseCode={this.state.courseCode}
                             onClick={i => this.handlesClick(i)}
+                            getNamelist={this.getNamelist}
+                            nameList={this.state.nameList}
+                            id={this.state.ID}
                         />
                         : null}
                 </div>
-            </div>
+            </div >
         );
     }
 
